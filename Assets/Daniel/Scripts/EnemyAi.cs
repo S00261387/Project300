bac0b10dbd385ef245c990cf79eb74ac03cd89c6
danielpolland <S00249853@atu.ie>
2025-10-20 21:48:23 +0100

    Added enemy animations

--- Assets/Daniel/Scripts/EnemyAi.cs
+++ Assets/Daniel/Scripts/EnemyAi.cs
@@ -21,15 +21,23 @@ public class EnemyAi : MonoBehaviour
     private float MoveCdTimer;
     Vector3 Home;
     public float HomeRadius;
+    public float PathUpdateDelay = 0.2f;
+    private float PathUpdateDeadline;
 
     //States
     public float sightRange;
     public bool playerInSightRange;
 
+    public Animator animator;
+
+    public bool Chasing;
+    public bool Attacking;
+
     private void Awake()
     {
         player = GameObject.Find("Player").transform;
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         Home = transform.position;
     }
 
@@ -37,13 +45,26 @@ public class EnemyAi : MonoBehaviour
     {
         playerInSightRange = Vector3.Distance(transform.position, player.transform.position) <= sightRange;
 
-        if (!playerInSightRange) Patrolling();
-        if (playerInSightRange) ChasePlayer();
+        if (!playerInSightRange)
+        { 
+            Chasing = false;
+            Patrolling(); 
+        }
+
+        if (playerInSightRange)
+        {
+            Chasing = true;
+            ChasePlayer(); 
+        }
 
         if (MoveCdTimer > 0)
         {
             MoveCdTimer -= Time.deltaTime;
         }
+
+        animator.SetFloat("Speed", agent.desiredVelocity.sqrMagnitude);
+        animator.SetBool("Chasing", Chasing);
+        animator.SetBool("Attacking", Attacking);
     }
 
     private void Patrolling()
@@ -76,21 +97,40 @@ public class EnemyAi : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Attacking = true;
+        }
+    }
 
-        private void SearchWalkPoint()
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Attacking = false;
+        }
+    }
+
+    private void SearchWalkPoint()
         {
             float randomZ = Random.Range(-walkPointRange, walkPointRange);
             float randomX = Random.Range(-walkPointRange, walkPointRange);
 
             walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
 
-            if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround) && Vector3.Distance(walkPoint, Home) <= HomeRadius)
+            if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround) && Vector3.Distance(walkPoint, Home) <= HomeRadius && Vector3.Distance(transform.position, walkPoint) >= walkPointRange/2)
                 walkPointSet = true;
 
         }
 
         private void ChasePlayer()
         {
+        if (Time.deltaTime >= PathUpdateDeadline)
+        {
+            PathUpdateDeadline = Time.deltaTime + PathUpdateDelay;
+        }
             agent.SetDestination(player.position);
         }
     
