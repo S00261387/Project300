d349f8562ee6e3f1990bf9f84e1da2513810eb83
Pierre-Louis Pozzo di Borgo <S00261387@atu.ie>
2025-12-07 20:06:55 +0000

    07/12 PL testing

--- Assets/PL/Scripts/Manager/SanityManager.cs
+++ Assets/PL/Scripts/Manager/SanityManager.cs
@@ -11,8 +11,8 @@ public class SanityManager : MonoBehaviour
     [Header("Sanity Settings")]
     public float sanity = 10f;          // current sanity
     public float maxSanity = 10f;       // max sanity cap (increases over time)
-    public float drainRateInDarkness = 2f;
-    public float recoverRateInLight = 1f;
+    public float drainRateInDarkness = 1f;
+    public float recoverRateInLight = 2f;
     public bool isInDarkness = true;
 
     [Header("Visual Effects")]
